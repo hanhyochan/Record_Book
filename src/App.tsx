68d9bd22f772d507faf26597cc185e346a026b0d
hanhyochan <hcartist569@gmail.com>
2025-04-26 16:43:48 +0900

    feat: MobileWriteBtn 공통 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import './App.css';
-import PrimaryBtn from './components/button/PrimaryBtn';
-import WriteBtn from './components/button/WriteBtn';
+import MobileWriteBtn from './components/button/MobileWriteBtn';
 
 function App() {
   return (
     <>
-      <PrimaryBtn tag="primaryBtnLg" disabled={true}>
-        등록하기
-      </PrimaryBtn>
-      <PrimaryBtn tag="primaryBtnMd" disabled={true}>
-        등록
-      </PrimaryBtn>
-      <PrimaryBtn tag="primaryBtnSm" disabled={true}>
-        등록
-      </PrimaryBtn>
-      <WriteBtn>글쓰기</WriteBtn>
+      <MobileWriteBtn>
+        <img src="/icon/Pencil.svg" alt="연필 아이콘" width="24" height="24" />
+      </MobileWriteBtn>
     </>
   );
 }
---
+++ src/components/button/MobileWriteBtn.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+
+interface MobileWriteBtnProps {
+  onClick?: () => void;
+  children: ReactNode;
+}
+
+const MobileWriteBtn = ({ onClick, children }: MobileWriteBtnProps) => {
+  return (
+    <button
+      className="w-[5.6rem] h-[5.6rem] bg-[#6025E1] rounded-[5rem] cursor-pointer flex items-center justify-center shadow-[0px_4px_5px_0px_rgba(159,159,159,0.25)]"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default MobileWriteBtn;
