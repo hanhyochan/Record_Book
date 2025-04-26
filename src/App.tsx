ce71a9d5e70dfe88c81a17b7b5c62a675d68723e
hanhyochan <hcartist569@gmail.com>
2025-04-26 15:51:29 +0900

    feat: PrimaryBtn 공통 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
-import Button from './components/button/Button';
+import PrimaryBtn from './components/button/PrimaryBtn';
 
 function App() {
-    return (
-        <>
-            <Button tag="primaryBtnLg">Primary Button Large</Button>
-        </>
-    );
+  return (
+    <>
+      <PrimaryBtn tag="primaryBtnLg" disabled={true}>
+        등록하기
+      </PrimaryBtn>
+      <PrimaryBtn tag="primaryBtnMd" disabled={true}>
+        등록
+      </PrimaryBtn>
+      <PrimaryBtn tag="primaryBtnSm" disabled={true}>
+        등록
+      </PrimaryBtn>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/button/PrimaryBtn.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react';
+
+interface PrimaryBtnProps {
+  tag: 'primaryBtnLg' | 'primaryBtnMd' | 'primaryBtnSm';
+  onClick?: () => void;
+  disabled?: boolean;
+  children: ReactNode;
+}
+
+const PrimaryBtn = ({
+  tag: Tag = 'primaryBtnLg',
+  onClick,
+  disabled = false,
+  children,
+}: PrimaryBtnProps) => {
+  // 기본 스타일
+  const baseClasses = {
+    primaryBtnLg: 'w-[20rem] h-[5.9rem] rounded-[1.2rem] px-[2.4rem] py-[1.6rem]',
+    primaryBtnMd: 'w-[8.9rem] h-[5.2rem] rounded-[1.2rem] px-[2.4rem] py-[1.2rem]',
+    primaryBtnSm: 'w-[8.4rem] h-[4.8rem] rounded-[1.2rem] px-[2.4rem] py-4',
+  };
+
+  // 활성화 스타일
+  const activeClasses = 'bg-black text-white cursor-pointer hover:bg-[#474953] active:bg-[#5E616E]';
+
+  // 비활성화 스타일
+  const inactiveClasses = 'bg-[#D6D7DC] text-[#A7A9B4] cursor-not-allowed';
+
+  return (
+    <button
+      className={`${baseClasses[Tag]} ${disabled ? inactiveClasses : activeClasses}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default PrimaryBtn;
